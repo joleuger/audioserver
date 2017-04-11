421ac437262e25a587a881ea63a58aa8390879c4
Johannes Leupolz <johannes.leupolz@googlemail.com>
2017-04-11 22:11:52 +0200

    added versioning

--- install-files/install/system-update.sh
+++ install-files/install/system-update.sh
@@ -1,1 +1,9 @@
 #!/bin/sh
+system_version=$(cat /settings/system.version)
+case $system_version in
+    1)
+        echo 'Latest version of system settings. No update required'
+        ;;
+    *)
+        echo 'Unknown version of system settings. No update performed'
+esac
--- install-files/install/user-update.sh
+++ install-files/install/user-update.sh
@@ -1,1 +1,9 @@
 #!/bin/sh
+user_version=$(cat /settings/user.version)
+case $system_version in
+    1)
+        echo 'Latest version of user settings. No update required'
+        ;;
+    *)
+        echo 'Unknown version of user settings. No update performed'
+esac
