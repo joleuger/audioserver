33ba5107bbcd886911bb374c8b5827db37de1371
Johannes Leupolz <johannes.leupolz@googlemail.com>
2017-04-19 22:27:52 +0200

    Improved

--- install-files/install/install.sh
+++ install-files/install/install.sh
@@ -88,7 +88,7 @@ cp target/release/librespot /usr/local/bin
 
 
 # setup mopidy
-apt-get install -qy python-dev python-gst-1.0 gir1.2-gstreamer-1.0 gir1.2-gst-plugins-base-1.0 gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-tools wget python-setuptools gstreamer1.0-pulseaudio
+apt-get install -qy python-dev python-gst-1.0 gir1.2-gstreamer-1.0 gir1.2-gst-plugins-base-1.0 gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-tools wget python-setuptools gstreamer1.0-pulseaudio libffi-dev
 cd /build
 wget https://pypi.python.org/packages/11/b6/abcb525026a4be042b486df43905d6893fb04f05aac21c32c638e939e447/pip-9.0.1.tar.gz#md5=35f01da33009719497f01a4ba69d63c9
 easy_install pip-9.0.1.tar.gz
