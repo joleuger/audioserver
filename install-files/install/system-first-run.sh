fe52318891371222247136d7a1e9a93309ac1c84
Johannes Leupolz <johannes.leupolz@googlemail.com>
2017-05-13 14:08:25 +0200

    minor improvement

--- install-files/install/system-first-run.sh
+++ install-files/install/system-first-run.sh
@@ -3,5 +3,4 @@ systemctl disable --now shairport-sync
 systemctl stop shairport-sync
 
 loginctl enable-linger audioserver
-rm -f /settings/firstrun ; rm -f /settings/firstrun.txt
 echo 1 > /settings/system.version
