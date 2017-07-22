9284d2f6dd4fbb4964cc08c7d5be58740896479a
Johannes Leupolz <johannes.leupolz@googlemail.com>
2017-07-22 17:30:44 +0200

    small change in daily script

--- install-files/settings/audioserver_restart_broadcast.sh
+++ install-files/settings/audioserver_restart_broadcast.sh
@@ -5,7 +5,7 @@ dd if=/run/user/1000/snapfifo iflag=nonblock of=/dev/null
 
 /bin/systemctl --user start audioserver_to_icecast.service audioserver_to_snapcast.service audioserver_snapcast.service audioserver_mopidy.service
 
-sleep 15
-/usr/bin/python3 /usr/local/bin/control_mopidy_dbus.py
+#sleep 15
+#/usr/bin/python3 /usr/local/bin/control_mopidy_dbus.py
 
 exit 0
