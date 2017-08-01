15c986ce3b724ce62134bd42a9d6972c8506917f
Johannes Leupolz <johannes.leupolz@googlemail.com>
2017-08-01 23:07:46 +0200

    switch to sample rate of 44100 for more performance on the raspberry pi

--- install-files/settings/audioserver_restart_broadcast.sh
+++ install-files/settings/audioserver_restart_broadcast.sh
@@ -8,4 +8,7 @@ dd if=/run/user/1000/snapfifo iflag=nonblock of=/dev/null
 #sleep 15
 #/usr/bin/python3 /usr/local/bin/control_mopidy_dbus.py
 
+BROADCAST_ON_LAN=$(pactl list short sinks | grep broadcast_on_lan | cut -f1)
+pactl set-sink-volume $BROADCAST_ON_LAN 70%
+
 exit 0
